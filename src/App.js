77605666424316d4b6cd4c40b2bec64bfcc64754
judithrauland <97590487+judithrauland@users.noreply.github.com>
2022-02-28 16:31:02 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <div className="footer">
-        <small>
-          <a
-            href="https://github.com/judithrauland/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>
-          {""} by Judith Rauland
-        </small>
+      <div className="container">
+        Weather App
+        <div className="footer">
+          <small>
+            <a
+              href="https://github.com/judithrauland/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>
+            {""} by Judith Rauland
+          </small>
+        </div>
       </div>
     </div>
   );
