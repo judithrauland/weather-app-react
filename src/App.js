6eee88a72b4c64c0a73bf6a2b62bb0d798f5e1fd
judithrauland <97590487+judithrauland@users.noreply.github.com>
2022-02-28 10:15:19 +0100

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <div className="footer">
+        <small>
+          <a
+            href="https://github.com/judithrauland/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          {""} by Judith Rauland
+        </small>
+      </div>
+    </div>
+  );
 }
